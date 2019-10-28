b19efa49a2e939329ef991387e148e278c7f01c3
myvin <myvinbarboza@gmail.com>
2019-10-28 19:10:59 +0530

    just array manipulation and linked stuff

---
+++ src/ReverseArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] a ={1,4,3,2};
+        int temp;
+        System.out.println(a.length);
+        for(int i=0;i<(a.length/2);i++){
+            temp=a[i];
+            a[i]=a[a.length-i-1];
+            a[a.length-1-i]=temp;
+
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
