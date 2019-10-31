7c934b816538abb60e5873622a8154be7192bb94
Barboza <737148@cognizant.com>
2019-10-31 12:39:05 +0530

    HOur glass complete

--- src/HourGlass.java
+++ src/HourGlass.java
@@ -23,14 +23,24 @@ public class HourGlass {
             { 0, 0, 1, 2, 4, 0 }
     };
     int sum=0;
-    int max=0;
+    int max= Integer.MIN_VALUE; /*Please take into consideration the posibility of negative integers*/
+    for(int r=0;r<4;r++){
         for (int k = 0; k < 4; k++) {
             for (int i=0 ; i <3 ; i++) {
                 for (int j =0; j <3 ; j++) {
-                    sum=sum+arr[i][j];
+                    sum=sum+arr[i+r][j+k];
+
                 }
             }
+            sum=sum-arr[r+1][k]-arr[r+1][k+2];
+            if(sum>max) max=sum;
+
+            sum=0;
+
         }
+
+    }
+        System.out.println(max);
 
     }
 
