291cfbe7f6cdca4dbe7495be1729456c2eec67b3
MyvinB <46648232+MyvinB@users.noreply.github.com>
2020-03-16 16:29:32 +0530

    Create ReverseStringLC344.java

---
+++ src/Array/ReverseStringLC344.java
@@ -0,0 +1,15 @@
+class ReverseStringLC344 {
+    public void reverseString(char[] s) {
+        int l=0;
+        int h=s.length-1;
+        while(l<h){
+            char temp=s[l];
+            s[l]=s[h];
+            s[h]=temp;
+            l++;
+            h--; 
+        }
+        
+        
+    }
+}
