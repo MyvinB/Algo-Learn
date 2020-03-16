c8fc4e3c46c4a864dce1409c61e53489edb3642c
MyvinB <46648232+MyvinB@users.noreply.github.com>
2020-03-16 18:01:18 +0530

    Update Node.java

--- src/Trees/Node.java
+++ src/Trees/Node.java
@@ -9,3 +9,16 @@ package Trees;
     Node right;
     Node(int x) { val = x; }
 }
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
