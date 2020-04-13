74820b0ba8e4fe75dff86c0b8cca26723571427f
myvin <myvinbarboza@gmail.com>
2020-04-13 08:06:35 +0530

    changes

--- src/Array/SearchInRotatedSortedArrayLC33.java
+++ src/Array/SearchInRotatedSortedArrayLC33.java
@@ -6,23 +6,41 @@ package Array;
  */
 public class SearchInRotatedSortedArrayLC33 {
 
-
-    static int search(int a[],int target){
-        if(a==null ||a.length==0)return -1;
-        int left=0;
-        int right =a.length-1;
-        while(left<right){
-            int midpoint=left+(right-left)/2;
-            if(a[midpoint]>a[right]){
-                left=midpoint+1;
-            }
-            else {
-                right=midpoint;
+    public static int findMin(int[] a) {
+        if(a.length==0)return -1;
+        //return value for only finmin problem a[0]
+        if(a.length==1)return 0;
+        int left = 0;
+        int right = a.length-1;
+            //code for duplicate elements
+        while(left<right&&a[left]==a[right]){
+            right--;
+        }
+        while (left<right){
+            int mid = left+(right-left)/2;
+            if(a[mid]>a[right]){
+                left=mid+1;
+            }else{
+                right=mid;
             }
         }
-        int start=left;
-        left=0;
-        right=a.length-1;
+        return left;
+    }
+
+
+
+
+
+    static int search(int a[],int target){
+        int start=findMin(a);
+        if(start==-1)return -1;
+        int left=0;
+        int right=a.length-1;
+        while(left<right&&a[left]==a[right]){
+            right--;
+        }
+
+
         if(target>=a[start] && target<= a[right]){
             left=start;
         } else {
@@ -39,9 +57,14 @@ public class SearchInRotatedSortedArrayLC33 {
     }
 
 
+
+
+
     public static void main(String[] args) {
         int a[]={4,5,6,7,0,1,2};
-        System.out.println(search(a,0));
+        int a1[]={1,2,1};
+        //System.out.println(findMin(a));
+        System.out.println(search(a1,2));
 
     }
 }
