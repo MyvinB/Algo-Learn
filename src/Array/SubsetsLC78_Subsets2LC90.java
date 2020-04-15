303dbe7003baca493806054c9ed434e78bf50e62
myvin <myvinbarboza@gmail.com>
2020-04-15 09:03:24 +0530

    update

--- src/Array/SubsetsLC78_Subsets2LC90.java
+++ src/Array/SubsetsLC78_Subsets2LC90.java
@@ -1,3 +1,5 @@
+package Array;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,9 +7,12 @@ import java.util.List;
 /*
  @author Myvin Barboza
  14/04/20 7:58 PM 
- */public class SubsetsLC78 {
+ */
+public class SubsetsLC78_Subsets2LC90 {
 
     static List<List<Integer>> subsets(int[] nums) {
+        //for unordered
+        Arrays.sort(nums);
      List<List<Integer>> list=new ArrayList<>();
      return recurSub(list,new ArrayList<>(),nums,0);
     }
@@ -15,6 +20,9 @@ import java.util.List;
     static List<List<Integer>> recurSub(List<List<Integer>> list,List temp,int[] nums,int start){
         list.add(new ArrayList<>(temp));
         for (int i = start; i <nums.length ; i++) {
+            //ignore this for LC78
+            //for duplicate elemets it basically sees if i > start and that number as occured before it's already in the list really simple..
+            if(i>start && nums[i]==nums[i-1])continue;;
             temp.add(nums[i]);
             recurSub(list,temp,nums,i+1);
             temp.remove(temp.size()-1);
@@ -27,7 +35,7 @@ import java.util.List;
 
 
     public static void main(String[] args) {
-        int num[]={1,2,3};
+        int num[]={1,2,2};
         System.out.println(subsets(num));
     }
 }
