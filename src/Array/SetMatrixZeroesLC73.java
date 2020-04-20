876d166a195a1b670e2b243a20ddd95a587a0131
myvin <myvinbarboza@gmail.com>
2020-04-20 13:39:39 +0530

    new files

---
+++ src/Array/SetMatrixZeroesLC73.java
@@ -0,0 +1,68 @@
+package Array;
+
+import java.util.Arrays;
+
+/*
+ @author Myvin Barboza
+ 20/04/20 8:02 AM 
+ */public class SetMatrixZeroesLC73 {
+
+    static void setZeroes(int[][] matrix) {
+        boolean fr=false,fc=false;
+        int m=matrix.length;
+        int n=matrix[0].length;
+
+        for (int i = 0; i <m ; i++) {
+            if(matrix[i][0]==0){
+                fc=true;
+                break;
+            }
+        }
+
+        for (int j = 0; j <n ; j++) {
+            if(matrix[0][j]==0){
+                fr=true;
+                break;
+            }
+        }
+
+        for (int i = 1; i <m ; i++) {
+            for (int j = 1; j <n ; j++) {
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for (int i = 1; i <m ; i++) {
+            for (int j = 1; j <n ; j++) {
+                if(matrix[i][0]==0  || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(fr){
+            for (int i = 0; i <n ; i++) {
+                matrix[0][i]=0;
+            }
+        }
+
+        if(fc){
+            for (int i = 0; i <m ; i++) {
+                matrix[i][0]=0;
+            }
+        }
+    }
+
+
+
+
+
+     public static void main(String[] args) {
+        int matrix[][]={{1,1,1},{0,1,1},{1,1,1}};
+         setZeroes(matrix);
+         System.out.println(Arrays.deepToString(matrix));
+    }
+}
---
+++ src/DP/HouseRobberLC198.java
@@ -0,0 +1,58 @@
+package DP;
+
+/*
+ @author Myvin Barboza
+ 20/04/20 11:39 AM 
+ */
+public class HouseRobberLC198 {
+
+    ///considering only robbeer 1 LC198
+    static int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        if(nums.length==2) return Math.max(nums[0],nums[1]);
+
+        int dp[]=new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for (int i = 2; i <nums.length ; i++) {
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+
+    static int robCircle(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        if(nums.length==2) return Math.max(nums[0],nums[1]);
+        int a=rob0(nums);
+        int b=rob1(nums);
+        return Math.max(a,b);
+    }
+
+    static int rob0(int[] nums){
+        int dp[]=new int[nums.length-1];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for (int i = 2; i <nums.length-1 ; i++) {
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[dp.length-1];
+    }
+
+    static int rob1(int[] nums){
+        int dp[]=new int[nums.length-1];
+        dp[0]=nums[1];
+        dp[1]=Math.max(nums[1],nums[2]);
+        for (int i = 2; i+1 <nums.length ; i++) {
+            dp[i]= Math.max(nums[i+1]+dp[i-2],dp[i-1]);
+        }
+        return dp[dp.length-1];
+    }
+
+
+    public static void main(String[] args) {
+        int nums[]={1,2,1,1};
+        System.out.println(robCircle(nums));
+    }
+}
