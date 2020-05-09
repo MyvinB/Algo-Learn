87cc7a5b38277ce08000bbf1ce434ecc31b40165
myvin <myvinbarboza@gmail.com>
2020-05-09 12:44:19 +0530

    codef

---
+++ src/CodeForce/A1.java
@@ -0,0 +1,19 @@
+//package CodeForce;
+
+import java.util.Scanner;
+
+/*
+ @author Myvin Barboza
+ 09/05/20 12:25 PM 
+ */public class A1 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double n=sc.nextDouble();
+        double m=sc.nextDouble();
+        double a=sc.nextDouble();
+       // System.out.println(Math.ceil(m/a));
+        double product=Math.ceil(m/a)*Math.ceil(n/a);
+        System.out.println((long)product);
+    }
+}
---
+++ src/CodeForce/A4.java
@@ -0,0 +1,17 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+/*
+ @author Myvin Barboza
+ 09/05/20 9:40 AM 
+ */public class A4 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+            if(n>2 && n%2==0) System.out.println("YES");
+            else System.out.println("NO");
+    }
+}
