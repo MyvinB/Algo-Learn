eda89ea905f1a6249c87bedba9ddb12db4c7b0c1
myvin <myvinbarboza@gmail.com>
2020-05-10 18:08:59 +0530

    cf2

---
+++ src/CodeForce/A231Team.java
@@ -0,0 +1,68 @@
+package CodeForce;
+/*
+ @author Myvin Barboza
+ 10/05/20 6:04 PM 
+ */
+import java.io.*;
+import java.util.*;
+
+public class A231Team {
+
+            /************************ SOLUTION STARTS HERE ***********************/
+                    private static void solve(FastScanner s1, PrintWriter out){
+                    int n=s1.nextInt();
+                    int ar[][]=new int[n][];
+                        for (int i = 0; i <n ; i++) {
+                            ar[i]=s1.nextIntArray(3);
+                        }
+                        int tcount=0;
+                        for (int i = 0; i < ar.length ; i++) {
+                            int count=0;
+                            for (int j = 0; j <ar[i].length ; j++) {
+                                if(ar[i][j]==1)count++;
+                            }
+                            if(count>=2)tcount++;
+                        }
+
+                        System.out.println(tcount);
+
+                    }
+
+                    /************************ SOLUTION ENDS HERE ************************/
+
+
+
+
+
+                    /************************ TEMPLATE STARTS HERE *********************/
+
+                    public static void main(String []args) throws IOException {
+                            FastScanner in  = new FastScanner(System.in);
+                            PrintWriter out =
+                                    new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+                            solve(in, out);
+                            in.close();
+                            out.close();
+                    }
+
+                    static class FastScanner{
+                            BufferedReader reader;
+                            StringTokenizer st;
+                            FastScanner(InputStream stream){reader=new BufferedReader(new InputStreamReader(stream));st=null;}
+                            String next()
+                            {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}
+                                    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+                            String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}
+                            int    nextInt()   {return Integer.parseInt(next());}
+                            long   nextLong()  {return Long.parseLong(next());}
+                            double nextDouble(){return Double.parseDouble(next());}
+                            char   nextChar()  {return next().charAt(0);}
+                            int[]  nextIntArray(int n)         {int[] arr= new int[n];   int i=0;while(i<n){arr[i++]=nextInt();}  return arr;}
+                            long[] nextLongArray(int n)        {long[]arr= new long[n];  int i=0;while(i<n){arr[i++]=nextLong();} return arr;}
+                            int[]  nextIntArrayOneBased(int n) {int[] arr= new int[n+1]; int i=1;while(i<=n){arr[i++]=nextInt();} return arr;}
+                            long[] nextLongArrayOneBased(int n){long[]arr= new long[n+1];int i=1;while(i<=n){arr[i++]=nextLong();}return arr;}
+                            void   close(){try{reader.close();}catch(IOException e){e.printStackTrace();}}
+                    }
+
+                    /************************ TEMPLATE ENDS HERE ************************/
+}
