6a73ea5f7b621051827c7c0f895b873bd725e0d5
myvin <myvinbarboza@gmail.com>
2020-05-15 13:41:09 +0530

    ty

---
+++ src/Challenge/Placement.java
@@ -0,0 +1,115 @@
+package Challenge;
+/*
+ @author Myvin Barboza
+ 14/05/20 4:18 PM 
+ */
+
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Placement {
+
+            /************************ SOLUTION STARTS HERE ***********************/
+            static String printInLine(HashMap<String,String> map,String s, long multi){
+                    if(!map.containsKey(s)) return multi+" "+s;
+                    String val[]=map.get(s).split(" ");
+                    long val1=Long.parseLong(val[0]);
+
+                    return printInLine(map,val[1],multi*val1);
+            }
+
+            static void modularise(HashMap<String,String> map,String type){
+
+            }
+
+            private static void solve(FastScanner s1, PrintWriter out){
+                        String s=s1.nextLine();
+                        String sArr[]=s.split(",");
+                        HashMap<String,String> map = new HashMap<String,String>();
+                        HashMap<Long,String> mapChange=new HashMap<>();
+                        TreeMap<Long,String> mapOrdered=new TreeMap<>();
+                        String lastVal="";
+                        long max=Long.MIN_VALUE;
+                        for (int i = 0; i <sArr.length-1 ; i++) {
+                                    String sKeyValue=s1.nextLine();
+                                    String sArr1[]=sKeyValue  .split(" = ");
+                                    String check[]=sArr1[1].split(" ");
+                                //    countMap.put(check[1],1+countMap.getOrDefault(check[1],0));
+                                    map.put(sArr1[0],sArr1[1]);
+                        }
+                        System.out.println(map);
+                        for(String key:map.keySet()){
+                                String val[]=map.get(key).split(" ");
+                                String newVal=printInLine(map,val[1],  Long.parseLong(val[0]));
+                               // System.out.println(newVal);
+                                String val1[]=newVal.split(" ");
+                                lastVal=val1[1];
+                                max=Math.max(max,Long.parseLong(val1[0]));
+                                mapChange.put(Long.parseLong(val1[0]),key);
+                        }
+                   System.out.println(max);
+                    System.out.println(mapChange);
+                        for(long key:mapChange.keySet()){
+                                String val=mapChange.get(key);
+                                long changedVal=max/key;
+                                mapOrdered.put(changedVal,val);
+                        }
+                    //System.out.println(mapOrdered);
+
+                    for (long key:mapOrdered.keySet()){
+                            System.out.print(key+""+mapOrdered.get(key)+" = ");
+                    }
+                    System.out.print(max+lastVal);
+
+
+
+
+
+
+//
+//
+
+
+
+                    }
+
+                    /************************ SOLUTION ENDS HERE ************************/
+
+
+
+
+
+                    /************************ TEMPLATE STARTS HERE *********************/
+
+                    public static void main(String []args) throws IOException {
+                            FastScanner in  = new FastScanner(System.in);
+                            PrintWriter out =
+                                    new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+                            solve(in, out);
+                            in.close();
+                            out.close();
+                    }
+
+                    static class FastScanner{
+                            BufferedReader reader;
+                            StringTokenizer st;
+                            FastScanner(InputStream stream){reader=new BufferedReader(new InputStreamReader(stream));st=null;}
+                            String next()
+                            {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}
+                                    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+                            String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}
+                            int    nextInt()   {return Integer.parseInt(next());}
+                            long   nextLong()  {return Long.parseLong(next());}
+                            double nextDouble(){return Double.parseDouble(next());}
+                            char   nextChar()  {return next().charAt(0);}
+                            int[]  nextIntArray(int n)         {int[] arr= new int[n];   int i=0;while(i<n){arr[i++]=nextInt();}  return arr;}
+                            long[] nextLongArray(int n)        {long[]arr= new long[n];  int i=0;while(i<n){arr[i++]=nextLong();} return arr;}
+                            int[]  nextIntArrayOneBased(int n) {int[] arr= new int[n+1]; int i=1;while(i<=n){arr[i++]=nextInt();} return arr;}
+                            long[] nextLongArrayOneBased(int n){long[]arr= new long[n+1];int i=1;while(i<=n){arr[i++]=nextLong();}return arr;}
+                            void   close(){try{reader.close();}catch(IOException e){e.printStackTrace();}}
+                    }
+
+                    /************************ TEMPLATE ENDS HERE ************************/
+
+}
