13926620208875375cbb59c4129cfa6b77a97b93
MyvinB <myvinbarboza@gmail.com>
2020-07-18 14:58:29 +0530

    correctin

--- src/LeetCode/Array/LC347TopKFrequentElements.java
+++ src/LeetCode/Array/LC347TopKFrequentElements.java
@@ -15,18 +15,15 @@ public class LC347TopKFrequentElements {
     static int[] topKFrequent(int[] nums, int k) {
         List<Integer> list=new ArrayList<Integer>();
         HashMap<Integer,Integer> map=new HashMap<>();
-        Set<Integer> set=new HashSet<>();
         for (int i = 0; i <nums.length ; i++) {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            set.add(nums[i]);
+
         }
         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
-        for(int a:set)pq.offer(a);
+        for(int a:map.keySet())pq.offer(a);
         while(k--!=0){
             list.add(pq.poll());
         }
-
-
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
