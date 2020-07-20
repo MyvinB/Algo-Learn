1c807f1a67f4e7c018d980ddcc952ca467a29838
MyvinB <myvinbarboza@gmail.com>
2020-07-20 12:33:19 +0530

    LC116

---
+++ src/LeetCode/Trees/LC116PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,72 @@
+package LeetCode.Trees;/*
+ @author Myvin Barboza
+ 20/07/20 12:15 PM 
+ */
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC116PopulatingNextRightPointersinEachNode {
+
+    class Node {
+        public int val;
+        public LeetCode.Trees.Node left;
+        public LeetCode.Trees.Node right;
+        public LeetCode.Trees.Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, LeetCode.Trees.Node _left, LeetCode.Trees.Node _right, LeetCode.Trees.Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+        //okay but specifically said constant space
+    public Node connectNO(Node root) {
+        if(root==null) return root;
+        Queue<Node> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                Node cur=queue.poll();
+                if(i!=size-1)cur.next=queue.peek();
+                if(cur.left!=null)queue.offer(cur.left);
+                if(cur.right!=null)queue.offer(cur.right);
+
+
+            }
+        }
+        return root;
+    }
+        /// 100 % faster constant space
+    public Node connect(Node root) {
+        // if(root==null) return;
+        //left most node of each level
+        Node curLevel=root;
+        while(curLevel!=null){
+            //setting of next pointer of  node in one level below
+            Node cur=curLevel;
+            while(cur!=null){
+                if(cur.left!=null)cur.left.next=cur.right;
+                if(cur.right!=null && cur.next!=null) cur.right.next=cur.next.left;
+                cur=cur.next;
+            }
+            curLevel=curLevel.left;
+        }
+        return root;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
