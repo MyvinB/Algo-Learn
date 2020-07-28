97ce19ac267c6abfdcc6fa51f802ee8b959c63a5
MyvinB <myvinbarboza@gmail.com>
2020-07-28 17:28:54 +0530

    LC621

---
+++ src/LeetCode/Array/LC621TaskScheduler.java
@@ -0,0 +1,35 @@
+package LeetCode.Array;/*
+ @author Myvin Barboza
+ 28/07/20 4:13 PM 
+ */
+
+public class LC621TaskScheduler {
+
+    static int leastInterval(char[] tasks, int n) {
+       int counter[]=new int[26];
+       int max=0;
+       int maxCount=0;
+       for(char ch:tasks){
+           counter[ch-'A']++;
+           if(max==counter[ch-'A']){
+               maxCount++;
+           }else if(max<counter[ch-'A']){
+               max=counter[ch-'A'];
+               maxCount=1;
+           }
+       }
+
+       int partCount=max-1;
+       int partLength=n-(maxCount-1);
+       int empty=partCount*partLength;
+       int availableTask= tasks.length-max*maxCount;
+       int idle=Math.max(0,empty-availableTask);
+       return tasks.length+idle;
+    }
+
+    public static void main(String[] args) {
+        char ch[]={'A','A','A','B','B','B'};
+        System.out.println(leastInterval(ch,0));
+    }
+
+}
