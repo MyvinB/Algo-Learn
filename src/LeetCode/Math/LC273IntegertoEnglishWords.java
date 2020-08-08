9772020768340176b78c77f620ffe5407583adaf
MyvinB <myvinbarboza@gmail.com>
2020-08-08 08:36:25 +0530

    LC237

---
+++ src/LeetCode/Math/LC273IntegertoEnglishWords.java
@@ -0,0 +1,42 @@
+package LeetCode.Math;/*
+ @author Myvin Barboza
+ 08/08/20 8:35 AM 
+ */
+
+public class LC273IntegertoEnglishWords {
+    public static void main(String[] args) {
+
+    }
+
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+
+    public String numberToWords(int num) {
+        if(num==0) return "Zero";
+        String words="";
+
+        int i=0;
+        while(num!=0){
+            if(num%1000!=0)
+                words=helper(num%1000)+THOUSANDS[i]+" "+words;
+            i++;
+            num=num/1000;
+        }
+        return  words.trim();
+    }
+
+    String helper(int num){
+        if(num==0)
+            return "";
+        if(num<20)
+            return LESS_THAN_20[num]+" ";
+        if(num<100)
+            return TENS[num/10]+" "+helper(num%10);
+
+        return LESS_THAN_20[num/100]+" Hundred "+helper(num%100);
+
+    }
+
+}
