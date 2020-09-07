e417524c89f012bbb620814b7e6e56d82d2acd6e
myvinb <myvinbarboza@gmail.com>
2020-09-07 20:41:58 +0530

    update

--- src/LeetCode/Array/LC835ImageOverlap.java
+++ src/LeetCode/Array/LC835ImageOverlap.java
@@ -8,34 +8,34 @@ public class LC835ImageOverlap {
 
     }
 
-  static int largestOverlap(int[][] A, int[][] B) {
-        int n=A.length;
+      static int largestOverlap(int[][] A, int[][] B) {
+            int n=A.length;
 
-        int count=0;
-        int max=0;
-        //calculated for shifts
-        for(int rowShift=-n;rowShift<n;rowShift++){
-            for(int colShift=-n;colShift<n;colShift++){
+            int count=0;
+            int max=0;
+            //calculated for shifts
+            for(int rowShift=0;rowShift<n;rowShift++){
+                for(int colShift=0;colShift<n;colShift++){
 
-               max=Math.max(maxVal(A,B,rowShift,colShift),max);
+                   max=Math.max(maxVal(A,B,rowShift,colShift),max);
+                }
             }
+            return max;
         }
-        return max;
-    }
 
-    static int maxVal(int[][] A,int[][] B,int x,int y){
-        int count=0;
-        int n=A.length;
-        for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                int iB=i+x;
-                int jB=i+y;
-                //to avoid Array out of bounds
-                if(iB<0 || iB>=A.length ||jB<0 ||jB>=A.length)continue;
+        static int maxVal(int[][] A,int[][] B,int x,int y){
+            int count=0;
+            int n=A.length;
+            for (int i = 0; i < n ; i++) {
+                for (int j = 0; j < n; j++) {
+                    int iB=i+x;
+                    int jB=j+y;
+                    //to avoid Array out of bounds
+                    if(iB<0 || iB>=A.length ||jB<0 ||jB>=A.length)continue;
 
-                if(A[i][j]==1 && B[iB][jB]==1)count++;
+                    if(A[i][j]==1 && B[iB][jB]==1)count++;
+                }
             }
+           return count;
         }
-       return count;
-    }
 }
