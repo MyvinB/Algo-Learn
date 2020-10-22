8d77b6b41f8f8f8ad1bead66595133092511bfe6
myvinb <myvinbarboza@gmail.com>
2020-10-22 21:35:22 +0530

    LC111

--- src/LeetCode/Trees/MinimumDepthOfBinaryTreeLC111.java
+++ src/LeetCode/Trees/MinimumDepthOfBinaryTreeLC111.java
@@ -1,5 +1,8 @@
 package LeetCode.Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MinimumDepthOfBinaryTreeLC111 {
 
     static int minDepth(Node root) {
@@ -10,6 +13,30 @@ public class MinimumDepthOfBinaryTreeLC111 {
         int left=minDepth(root.left);
         int right=minDepth(root.right);
         return Math.min(left,right)+1;
+    }
+    /*
+    Simple bfs count levels increment just return depth when we reach leaf node
+     */
+
+    static  int minDepthNew(TreeNode root){
+        if(root==null) return 0;
+        int depth=1;
+        Queue<TreeNode> queue =new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node= queue.poll();
+                if(node.left==null && node.right==null)
+                    return depth;
+                if(node.left!=null)
+                    queue.offer(node.left);
+                if(node.right!=null)
+                    queue.offer(node.right);
+            }
+            depth++;
+        }
+        return -1;
     }
     
     public static void main(String[] args) {
