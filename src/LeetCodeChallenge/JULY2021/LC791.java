4c312766caa771f9b64863369123b21704a1ab9d
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-07-15 08:36:01 +0530

    LC791

---
+++ src/LeetCodeChallenge/JULY2021/LC791.java
@@ -0,0 +1,32 @@
+package LeetCodeChallenge.JULY2021;
+
+public class LC791 {
+
+    public static void main(String[] args) {
+        customSortString("cba", "abcd");
+    }
+
+    public static String customSortString(String order, String str) {
+        StringBuilder sb = new StringBuilder();
+        int[] ar = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            ar[str.charAt(i) - 'a'] += 1;
+        }
+        for (int i = 0; i < order.length(); i++) {
+            char cur = order.charAt(i);
+            while (ar[cur - 'a'] > 0) {
+                sb.append(cur);
+                ar[cur - 'a']--;
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char cur = str.charAt(i);
+            while (ar[cur - 'a'] > 0) {
+                sb.append(cur);
+                ar[cur - 'a']--;
+            }
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+}
