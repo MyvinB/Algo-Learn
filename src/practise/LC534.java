1d9b445a262d45202cd26721f0e7e99db5e67f1b
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-10-11 14:24:33 +0530

    updating

--- src/practise/LC534.java
+++ src/practise/LC534.java
@@ -1,2 +1,20 @@
-package practise;public class LC534 {
+package practise;
+
+public class LC534 {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        findMax(root);
+        return max;
+    }
+
+    int findMax(TreeNode root){
+        if(root == null) return 0;
+
+        int left = findMax(root.left);
+        int right = findMax(root.right);
+        //whole tree including root node at every level
+        max = Math.max(max,left+right);
+        //think height of tree;
+        return 1 + Math.max(left,right);
+    }
 }
