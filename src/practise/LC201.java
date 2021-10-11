c3934fc3554f4ab857fb509c81a46709b2a8d3b3
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-10-11 14:22:23 +0530

    LC534

--- src/practise/LC201.java
+++ src/practise/LC201.java
@@ -1,2 +1,14 @@
-package practise;public class LC201 {
+package practise;
+
+public class LC201 {
+
+    public int rangeBitwiseAnd(int m, int n) {
+        int c =0;
+        while(m!=n){
+            m=m>>1;
+            n=n>>1;
+            c++;
+        }
+        return m <<c;
+    }
 }
---
+++ src/practise/LC534.java
@@ -0,0 +1,2 @@
+package practise;public class LC534 {
+}
