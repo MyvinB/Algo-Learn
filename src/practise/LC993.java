ceea470f5eee4b9c95ad7cf4e95ba534fcb614d2
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-10-19 23:10:19 +0530

    LC496

--- src/practise/LC993.java
+++ src/practise/LC993.java
@@ -1,2 +1,28 @@
-package practise;public class LC993 {
+package practise;
+
+public class LC993 {
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(depth(root,x)==depth(root,y) && parents(root,x)!=parents(root,y)) return true;
+        return false;
+    }
+
+    public static TreeNode parents(TreeNode root,int data){
+        if(root==null) return null;
+        if(root.left!=null && root.left.val==data) return root;
+        if(root.right!=null && root.right.val==data) return root;
+
+        TreeNode left=parents(root.left,data);
+        if(left!=null) return left;
+        return parents(root.right,data);
+
+    }
+
+    public static int depth(TreeNode root,int data){
+
+        if(root==null) return Integer.MIN_VALUE;
+        if(root.val==data) return 0;
+
+        return 1+Math.max(depth(root.left,data),depth(root.right,data));
+    }
 }
