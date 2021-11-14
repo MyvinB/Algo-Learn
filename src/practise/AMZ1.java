f896ae6f6df22ab0dd1bda0441873aef87a3615c
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-11-14 10:43:45 +0530

    AMZ

---
+++ src/practise/AMZ1.java
@@ -0,0 +1,27 @@
+package practise;
+
+public class AMZ1 {
+    public static void main(String args[]) {
+        System.out.println(minimumNumber(4321));
+    }
+
+    public static int minimumNumber(int n){
+        String s =""+n;
+        int[] a = new int[s.length()];
+        for (int i = 0; i <s.length() ; i++) {
+            a[i] = Integer.parseInt(""+s.charAt(i));
+        }
+        Arrays.sort(a);
+        int p1=0,p2=0;
+        for (int i = 0; i <a.length ; i++) {
+            if(i%2==0){
+                p1=p1*10+a[i];
+            }else{
+                p2=p2*10+a[i];
+            }
+        }
+
+        return p1+p2;
+    }
+
+}
--- src/practise/AMZ2.java
+++ src/practise/AMZ2.java
@@ -1,12 +1,14 @@
 package practise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AMZ2 {
 
     public static void main(String args[]) {
-        System.out.println(minMoves(12, new int[]{20,14,13,15,14,18,10,14,17,10,14,11}, 6));
+//        System.out.println(minMoves(12, new int[]{20,14,13,15,14,18,10,14,17,10,14,11}, 6));
+                System.out.println(minMoves(5, new int[]{1,2,0,1,2}, 3));
     }
 
     public static int minMoves(int n, int[] arr, int k) {
@@ -16,7 +18,6 @@ public class AMZ2 {
             for (int j = i; j < n; j += k) {
                 list.add(arr[j]);
             }
-
             count +=(list.size() - lengthOfLIS(list.stream().mapToInt(Integer::intValue).toArray()));
         }
         return count;
@@ -30,11 +31,12 @@ public class AMZ2 {
             int i = 0, j = size;
             while (i != j) {
                 int m = (i + j) / 2;
-                if (tails[m] < x)
+                if (tails[m] <= x)
                     i = m + 1;
                 else
                     j = m;
             }
+            System.out.println(Arrays.toString(tails));
             tails[i] = x;
             if (i == size) ++size;
         }
