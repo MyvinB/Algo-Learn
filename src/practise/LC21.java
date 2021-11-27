63bedf8ecc0146615f9a4964ceb284825133f9bf
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-11-27 09:35:11 +0530

    LC21

---
+++ src/practise/LC21.java
@@ -0,0 +1,53 @@
+package practise;
+
+/**
+ * @author mmichaelbarboza
+ * on 9:07 AM 11/27/2021
+ */
+public class LC21 {
+
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        l2.next= new ListNode(3);
+        l2.next.next = new ListNode(4);
+        mergeTwoLists(l1,l2);
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        int val = 0;
+        while(true){
+            if(list1 == null){
+                dummy.next=list2;
+                break;
+            }
+            if(list2 == null){
+                dummy.next=list1;
+                break;
+            }
+            if(list1.val<=list2.val){
+                dummy.next = list1;
+                list1=list1.next;
+
+            }else {
+                dummy.next= list2;
+                list2= list2.next;
+            }
+            dummy = dummy.next;
+        }
+
+        return dummy.next;
+
+    }
+}
