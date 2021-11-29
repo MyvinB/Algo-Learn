23cc6b96e8f3659b7f670f8156d00678ea8bacca
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-11-29 09:36:21 +0530

    LC696

--- src/practise/LC1010.java
+++ src/practise/LC1010.java
@@ -18,15 +18,13 @@ import java.util.HashMap;
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<time.length;i++){
             if(time[i]%60 == 0){
-                res += map.get(time[i]%60);
+                res += map.getOrDefault(time[i]%60,0);
             }
             else res+= map.getOrDefault(60-time[i]%60,0);
 
             map.put(time[i]%60,map.getOrDefault(time[i]%60,0)+1);
         }
-
         return res;
-
     }
 
 
---
+++ src/practise/LC696.java
@@ -0,0 +1,5 @@
+package practise;/**
+ *@author mmichaelbarboza
+ * on 8:42 AM 11/29/2021
+ */public class LC696 {
+}
