f55cb0addc6b4624be1aa78d4385314b0ce56f0a
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-12-01 18:20:26 +0530

    LC973

---
+++ src/practise/LC973.java
@@ -0,0 +1,32 @@
+package practise;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author mmichaelbarboza
+ * on 6:05 PM 12/1/2021
+ */
+public class LC973 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p1,p2)->p2[0]*p2[0]+p2[1]*p2[1] - p1[0]*p1[0]+p1[1]*p1[1]);
+        for(int i=0;i<points.length;i++){
+            pq.offer(points[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[][] res = new int[k][];
+        int i=0;
+        while(!pq.isEmpty()){
+            res[i++] = pq.poll();
+        }
+        return res;
+
+
+    }
+}
