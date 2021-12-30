0cc37695f24eb519d42a3bba59abfa85f735cb54
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2021-12-30 22:34:45 +0530

    LC1015

---
+++ src/LeetCodeChallenge/December2021/LC1015.java
@@ -0,0 +1,22 @@
+package LeetCodeChallenge.December2021;
+
+/**
+ * @author mmichaelbarboza
+ * on 10:26 PM 12/30/2021
+ */
+public class LC1015 {
+
+    public static void main(String[] args) {
+        System.out.println(smallestRepunitDivByK(4));
+    }
+
+    public static int smallestRepunitDivByK(int k) {
+        if(k==2 || k==5) return -1;
+        int remainder =0;
+        for(int i=1;i<=k;i++){
+            remainder = (remainder*10+1)%k;
+            if(remainder==0) return i;
+        }
+        return -1;
+    }
+}
