b4738a064a7e1d5887ad371bd7fae749f31a3546
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-01-10 08:14:39 +0530

    LC67

--- src/LeetCodeChallenge/JAN2022/LC1041.java
+++ src/LeetCodeChallenge/JAN2022/LC1041.java
@@ -10,6 +10,24 @@ public class LC1041 {
     }
 
     public boolean isRobotBounded(String instructions) {
-        return true;
+        //Imp logic 1 left = 3 right or 1 right = 3left
+        //if a circle the direction will never point in the same
+        //GLLLL
+        //
+        int[][] dir = new int[][]{{0,1},{-1,0},{0,-1},{1,0}};
+        int k=0;
+        int x=0,y=0;
+        for(int i=0;i<instructions.length();i++){
+            if(instructions.charAt(i)=='L'){
+                k=(k+1)%4;
+            }else if(instructions.charAt(i)=='R'){
+                k=(k+3)%4;
+            }
+            else{
+                x+=dir[k][0];
+                y+=dir[k][1];
+            }
+        }
+        return x==0 && y==0 ;
     }
 }
---
+++ src/LeetCodeChallenge/JAN2022/LC67.java
@@ -0,0 +1,32 @@
+package LeetCodeChallenge.JAN2022;
+
+/**
+ * @author mmichaelbarboza
+ * on 7:30 AM 1/10/2022
+ */
+public class LC67 {
+    public static void main(String[] args) {
+        System.out.println(addBinary("11","11"));
+    }
+
+    public static String addBinary(String a, String b) {
+    StringBuilder sb = new StringBuilder();
+    int m = a.length()-1;
+    int n = b.length()-1;
+    int carry = 0;
+    int sum = 0;
+    int no1 =0;
+    int no2 = 0;
+    while(true){
+        if(m<0 && n<0) break;
+        no1 = m>=0?a.charAt(m)-'0':0;
+        no2 = n>=0?b.charAt(n)-'0':0;
+        sum= no1+no2+carry;
+        sb.append(sum%2);
+        carry = sum/2;
+        m--;n--;
+    }
+    if(carry!=0) sb.append(carry);
+    return sb.reverse().toString();
+    }
+}
