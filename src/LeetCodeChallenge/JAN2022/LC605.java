480d67aad86d77cf4d988fde489f44212d3e4b67
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-01-18 11:30:38 +0530

    LC605

---
+++ src/LeetCodeChallenge/JAN2022/LC605.java
@@ -0,0 +1,5 @@
+package LeetCodeChallenge.JAN2022;/**
+ *@author mmichaelbarboza
+ * on 11:22 AM 1/18/2022
+ */public class LC605 {
+}
--- src/LeetCodeChallenge/JAN2022/practise/LC1920.java
+++ src/LeetCodeChallenge/JAN2022/practise/LC1920.java
@@ -1,5 +1,23 @@
-package LeetCodeChallenge.JAN2022.practise;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.JAN2022.practise;
+
+/**
+ * @author mmichaelbarboza
  * on 10:01 AM 1/11/2022
- */public class LC1920 {
+ */
+public class LC1920 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<nums.length;i++){
+            nums[i] = nums[i] + n*(nums[nums[i]]%n);
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i] = nums[i]/n;
+        }
+        return nums;
+    }
 }
