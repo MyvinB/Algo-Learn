ee9d228d934124e9b939f561928ae126cfb9a3fb
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-01-20 11:57:58 +0530

    LC875

---
+++ src/LeetCodeChallenge/JAN2022/LC875.java
@@ -0,0 +1,30 @@
+package LeetCodeChallenge.JAN2022;
+
+/**
+ * @author mmichaelbarboza
+ * on 11:23 AM 1/20/2022
+ */
+public class LC875 {
+
+    public static void main(String[] args) {
+        int[] piles = new int[]{30,11,23,4,20};
+        System.out.println(minEatingSpeed(piles,6));
+    }
+
+    public static int minEatingSpeed(int[] piles, int h) {
+        int l=1;
+        int r=30;
+        while(l<r){
+            int m = (r+l)/2;
+            int hr=0;
+            for(int i=0;i<piles.length;i++){
+                hr+=Math.ceil((double)piles[i]/m);
+            }
+            if(hr>h){
+                l=m+1;
+            }
+            else r=m;
+        }
+        return l;
+    }
+}
---
+++ src/LeetCodeChallenge/JAN2022/practise/LC556.java
@@ -0,0 +1,5 @@
+package LeetCodeChallenge.JAN2022.practise;/**
+ *@author mmichaelbarboza
+ * on 2:54 PM 1/19/2022
+ */public class LC556 {
+}
