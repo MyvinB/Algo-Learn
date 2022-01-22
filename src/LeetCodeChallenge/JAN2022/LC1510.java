07fff1b3ca064ee93320523ad4fee0a9d12b1541
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-01-22 15:52:57 +0530

    LC1510

---
+++ src/LeetCodeChallenge/JAN2022/LC1510.java
@@ -0,0 +1,24 @@
+package LeetCodeChallenge.JAN2022;
+
+/**
+ * @author mmichaelbarboza
+ * on 3:40 PM 1/22/2022
+ */
+public class LC1510 {
+    public static void main(String[] args) {
+        System.out.println(winnerSquareGame(7));
+    }
+
+    public static boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 1; k * k <= i; ++k) {
+                if (!dp[i - k * k]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
--- src/LeetCodeChallenge/JAN2022/LC556.java
+++ src/LeetCodeChallenge/JAN2022/LC556.java
@@ -14,6 +14,7 @@ public class LC556 {
 
 
     public int nextGreaterElement(int n) {
+        //https://www.youtube.com/watch?v=eAfJJyaAmes&t=1002
         char[] num = (""+n).toCharArray();
         int i,j;
         for(i=num.length-1;i>0;i--){
@@ -24,16 +25,17 @@ public class LC556 {
         //find the decreasing number
         int x = num[i-1];
         int smallest = i;
+        //Find the next greatest number
         for(j=i+1;j<num.length;j++) {
             if(num[j]>x && x< num[smallest]) {
                 smallest=j;
             }
         }
-
+        //exchange
         char temp = num[i-1];
         num[i-1] = num[smallest];
         num[smallest] = temp;
-
+        //sort the remaining
         Arrays.sort(num,i,num.length);
         long val = Long.parseLong(new String(num));
         return val<=Integer.MAX_VALUE? (int)val:-1;
@@ -41,5 +43,5 @@ public class LC556 {
         }
 
 
-    }
+
 }
