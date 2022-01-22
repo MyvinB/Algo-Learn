7bef7d22b472c8d8b336629f8a8ee3346e3457fd
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-01-22 15:53:09 +0530

    .

--- src/LeetCodeChallenge/JAN2022/LC1510.java
+++ src/LeetCodeChallenge/JAN2022/LC1510.java
@@ -11,12 +11,13 @@ public class LC1510 {
 
     public static boolean winnerSquareGame(int n) {
         boolean[] dp = new boolean[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            for (int k = 1; k * k <= i; ++k) {
-                if (!dp[i - k * k]) {
-                    dp[i] = true;
+        for(int i=1;i<=n;i++){
+            for(int k=1;k*k<=i;k++){
+                if(!dp[i-k*k]){
+                    dp[i]= true;
                     break;
                 }
+
             }
         }
         return dp[n];
