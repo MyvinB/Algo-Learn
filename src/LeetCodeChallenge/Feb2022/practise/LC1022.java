b9807119279a5140128e29dfcdcff6d080295057
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-02-14 16:07:25 +0530

    LC1022

--- src/LeetCodeChallenge/Feb2022/practise/LC1022.java
+++ src/LeetCodeChallenge/Feb2022/practise/LC1022.java
@@ -2,10 +2,10 @@ package LeetCodeChallenge.Feb2022.practise;
 
 /**
  * @author mmichaelbarboza
- * on 12:16 PM 2/14/2022
- * Time Taken: 2 min
+ * on 3:43 PM 2/14/2022
+ * Time Taken:
  */
-public class LC700 {
+public class LC1022 {
 
     public class TreeNode {
         int val;
@@ -20,14 +20,18 @@ public class LC700 {
         }
     }
 
+
     public static void main(String[] args) {
 
     }
 
-    public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null) return null;
-        if(root.val>val) return searchBST(root.left,val);
-        if(root.val<val) return searchBST(root.right,val);
-        return root;
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root,0);
+    }
+
+    public int dfs(TreeNode root,int val){
+        if(root==null)return 0;
+        val =val*2+root.val;
+        return root.left==root.right?val: dfs(root.left,val)+dfs(root.right,val);
     }
 }
