be57eae6dce2b5c143f394f291377a908d5d3ead
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-02-15 16:40:54 +0530

    update

--- src/LeetCodeChallenge/Feb2022/LC136.java
+++ src/LeetCodeChallenge/Feb2022/LC136.java
@@ -1,6 +1,17 @@
-package LeetCodeChallenge.Feb2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.Feb2022;
+
+/**
+ * @author mmichaelbarboza
  * on 4:39 PM 2/15/2022
- * Time Taken: 
- */public class LC136 {
+ * Time Taken:
+ */
+public class LC136 {
+
+    public int singleNumber(int[] nums) {
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            res ^= nums[i];
+        }
+        return res;
+    }
 }
