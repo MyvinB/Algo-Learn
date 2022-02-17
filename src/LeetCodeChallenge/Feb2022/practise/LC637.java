6e2ff343e13edf593a4ae2f4ff0d58c6e0463de7
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-02-17 09:27:41 +0530

    LC637

---
+++ src/LeetCodeChallenge/Feb2022/practise/LC637.java
@@ -0,0 +1,49 @@
+package LeetCodeChallenge.Feb2022.practise;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author mmichaelbarboza
+ * on 9:19 AM 2/17/2022
+ * Time Taken:
+ */
+public class LC637 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+
+        public static void main(String[] args) {
+
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            double sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode t = queue.poll();
+                if(t.left!=null) queue.add(t);
+                if(t.right!=null) queue.add(t);
+                sum += t.val;
+            }
+            res.add(sum/size);
+        }
+        return res;
+    }
+}
--- src/LeetCodeChallenge/Feb2022/practise/LC965.java
+++ src/LeetCodeChallenge/Feb2022/practise/LC965.java
@@ -1,6 +1,28 @@
-package LeetCodeChallenge.Feb2022.practise;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.Feb2022.practise;
+
+/**
+ * @author mmichaelbarboza
  * on 9:11 AM 2/17/2022
- * Time Taken: 
- */public class LC965 {
+ * Time Taken:
+ */
+public class LC965 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if(root==null) return true;
+        if(root.left!=null && root.left.val!=root.val) return false;
+        if(root.right!=null && root.right.val!=root.val) return false;
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
 }
