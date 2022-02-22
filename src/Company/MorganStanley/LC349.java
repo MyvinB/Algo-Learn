7298678d37760d58df8ec18d9d21f7738aa876c3
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-02-22 10:42:42 +0530

    LC349

---
+++ src/Company/MorganStanley/LC349.java
@@ -0,0 +1,6 @@
+package Company.MorganStanley;/**
+ *@author mmichaelbarboza
+ * on 10:37 AM 2/22/2022
+ * Time Taken: 
+ */public class LC349 {
+}
--- src/Company/MorganStanley/LC53.java
+++ src/Company/MorganStanley/LC53.java
@@ -1,6 +1,25 @@
-package Company.MorganStanley;/**
- *@author mmichaelbarboza
+package Company.MorganStanley;
+
+/**
+ * @author mmichaelbarboza
  * on 8:59 AM 2/22/2022
- * Time Taken: 
- */public class LC53 {
+ * Time Taken:
+ */
+public class LC53 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxSubArray(int[] nums) {
+        int preSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            preSum+=nums[i];
+            maxSum = Math.max(maxSum,preSum);
+            //If presum is - why have it in your subarray it would decrease its size
+            if(preSum<0) preSum=0;
+        }
+        return maxSum;
+    }
 }
