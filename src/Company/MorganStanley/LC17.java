2974ef5e98e2cb4c0d5ca98140acd2465cfc4a27
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-02-24 11:07:13 +0530

    LC17

---
+++ src/Company/MorganStanley/LC17.java
@@ -0,0 +1,33 @@
+package Company.MorganStanley;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author mmichaelbarboza
+ * on 8:48 AM 2/24/2022
+ * Time Taken:
+ */
+public class LC17 {
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        if(digits.isEmpty()) return ans;
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        for(int i =0; i<digits.length();i++){
+            int x = Character.getNumericValue(digits.charAt(i));
+            while(ans.peek().length()==i){
+                String t = ans.remove();
+                for(char s : mapping[x].toCharArray())
+                    ans.add(t+s);
+            }
+        }
+        return ans;
+    }
+}
