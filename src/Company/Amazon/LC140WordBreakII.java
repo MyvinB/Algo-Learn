7935783ef535048924401a47ced6918fb1f632b9
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-03-13 07:57:40 +0530

    LC140 Word Break 2

---
+++ src/Company/Amazon/LC140WordBreakII.java
@@ -0,0 +1,43 @@
+package Company.Amazon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author mmichaelbarboza
+ * on 6:55 AM 3/13/2022
+ * Time Taken:
+ */
+public class LC140WordBreakII {
+
+    public static void main(String[] args) {
+        String s = "a";
+        String[] ar = {"b","cats","and","sand","dog"};
+        System.out.println(wordBreak(s, Arrays.asList(ar)));
+    }
+
+    public static HashMap<String,List<String>> map=new HashMap<>();
+    public static List<String> wordBreak(String s, List<String> wordDict) {
+      List<String> res = new ArrayList<>();
+        if(s==null || s.length() == 0)
+            return res;
+      if(map.containsKey(s)) return map.get(s);
+      if(wordDict.contains(s)) res.add(s);
+
+      for(int i=1;i<s.length();i++){
+          String t = s.substring(i);
+          if(wordDict.contains(t)){
+              List<String> temp = wordBreak(s.substring(0,i),wordDict);
+              if(temp!=null){
+                  for(int j=0;j<temp.size();j++){
+                      res.add(temp.get(j) +" "+t);
+                  }
+              }
+          }
+      }
+      map.put(s,res);
+      return res;
+    }
+}
