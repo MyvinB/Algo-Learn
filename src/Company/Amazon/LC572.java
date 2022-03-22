d6f78c6693520da009e7d1afd62e84cb765f3fe6
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-03-22 09:02:07 +0530

    LC572 check

---
+++ src/Company/Amazon/LC572.java
@@ -0,0 +1,6 @@
+package Company.Amazon;/**
+ *@author mmichaelbarboza
+ * on 8:45 AM 3/22/2022
+ * Time Taken: 
+ */public class LC572 {
+}
--- src/Company/Amazon/LC973.java
+++ src/Company/Amazon/LC973.java
@@ -1,6 +1,32 @@
-package Company.Amazon;/**
- *@author mmichaelbarboza
+package Company.Amazon;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author mmichaelbarboza
  * on 8:16 AM 3/22/2022
- * Time Taken: 
- */public class LC973 {
+ * Time Taken:
+ */
+public class LC973 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        //max heap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->((b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1])));
+        for(int i=0;i<points.length;i++){
+            pq.offer(points[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[][] res = new int[k][2];
+        int r =0;
+        while(!pq.isEmpty()){
+            res[r++] = pq.poll();
+        }
+        return res;
+    }
 }
