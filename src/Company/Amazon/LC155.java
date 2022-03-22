f7c8bde0f3f14a4c928036e69e2c5387b4b05ea1
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-03-22 10:40:10 +0530

    Time pass

--- src/Company/Amazon/LC155.java
+++ src/Company/Amazon/LC155.java
@@ -10,49 +10,52 @@ import java.util.Stack;
 public class LC155 {
 
     public static void main(String[] args) {
+        //Can also be done by having the min in stack whenever there is min value
 
     }
 
     class MinStack {
-        Stack<Integer> stack;
-        int min;
+        Stack<Long> stack;
+        Long min;
         public MinStack() {
             stack = new Stack<>();
-            min = Integer.MAX_VALUE;
+            min = Long.MAX_VALUE;
         }
 
         public void push(int val) {
+            Long value = Long.valueOf(val);
             if(val<min){
-                min = val;
                 //value will always be less as
                 //val<min
                 // val-min<0
                 //2val-min<val
-                stack.push(2*val-min);
+                stack.push(2*value-min);
+                min = value;
+
             }
-            else stack.push(val);
+            else stack.push(value);
         }
 
         public void pop() {
-            int val = stack.pop();
+            if(stack.isEmpty()) return;
+            long val = stack.pop();
             //top = 2*val - prevMin
             //2val - (2*Val -prevMin)
             //prevMin
             //val over here is current min
-            if(val<min){
+            if(val<min) {
                 min = 2 * min - val;
             }
-
-
         }
 
         public int top() {
-            if(stack.peek()<min) return min;
-            else return stack.peek();
+            if(stack.isEmpty()) return -1;
+            if(stack.peek()<min) return min.intValue();
+            else return stack.peek().intValue();
         }
 
         public int getMin() {
-            return min;
+            return min.intValue();
         }
     }
 }
---
+++ src/Company/Amazon/LC206.java
@@ -0,0 +1,6 @@
+package Company.Amazon;/**
+ *@author mmichaelbarboza
+ * on 10:36 AM 3/22/2022
+ * Time Taken: 
+ */public class LC206 {
+}
