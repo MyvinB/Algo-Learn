db175a260e544733225e5ca16563401a34be6274
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-03-22 11:11:20 +0530

    Learnt usage of \\W+ and \\s+

--- src/Company/Amazon/LC206.java
+++ src/Company/Amazon/LC206.java
@@ -1,6 +1,30 @@
-package Company.Amazon;/**
- *@author mmichaelbarboza
+package Company.Amazon;
+
+/**
+ * @author mmichaelbarboza
  * on 10:36 AM 3/22/2022
- * Time Taken: 
- */public class LC206 {
+ * Time Taken:
+ */
+public class LC206 {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode next = null;
+        ListNode prev = null;
+        while(cur!=null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
 }
