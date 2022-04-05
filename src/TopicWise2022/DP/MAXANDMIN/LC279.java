1e09c2d7cc7845d9f7c580a24d7878d4f0c03ce1
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-05 22:45:51 +0530

    DP +4 Coin Change problem

--- src/TopicWise2022/DP/MAXANDMIN/LC279.java
+++ src/TopicWise2022/DP/MAXANDMIN/LC279.java
@@ -13,12 +13,14 @@ public class LC279 {
     }
 
     public static int numSquares(int n) {
-        int dp[]=new int[n+1];
-//        Arrays.fill(dp, );
-        dp[0]=0;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
         for(int i=1;i<=n;i++){
+            //Here we need if squares or not so we traverse till half 2*2<=4 when we find a case like this ie number is a perfect square which is then dp[0]+1 which is 1
             for(int j=1;j*j<=i;j++){
-                dp[i]=Math.min(dp[i],1+dp[i-j*j]);
+                // here we minus a perfect square and see the prev result see we have one perfect square say when j =2 ie 4 or j=3 ie 9 since we minus that we add one
+                dp[i] = Math.min(dp[i],1 +dp[i-j*j]);
             }
         }
         return dp[n];
---
+++ src/TopicWise2022/DP/MAXANDMIN/LC322.java
@@ -0,0 +1,30 @@
+package TopicWise2022.DP.MAXANDMIN;
+
+import java.util.Arrays;
+
+/**
+ * @author mmichaelbarboza
+ * on 10:28 PM 4/5/2022
+ * Time Taken:
+ */
+public class LC322 {
+    public static void main(String[] args) {
+        System.out.println(coinChange(new int[]{2},3));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                int balance = i- coins[j];
+                if(balance>=0){
+                    dp[i] = Math.min(dp[i],1+dp[balance]);
+                }
+            }
+        }
+
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
+    }
+}
