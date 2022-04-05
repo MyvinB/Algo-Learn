3f1c9d2fa7019c14d2d60f60e912c56fffcc2de0
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-05 12:41:41 +0530

    Daily challenge

---
+++ src/LeetCodeChallenge/APR2022/LC11.java
@@ -0,0 +1,6 @@
+package LeetCodeChallenge.APR2022;/**
+ *@author mmichaelbarboza
+ * on 12:36 PM 4/5/2022
+ * Time Taken: 
+ */public class LC11 {
+}
--- src/TopicWise2022/DP/MAXANDMIN/LC120.java
+++ src/TopicWise2022/DP/MAXANDMIN/LC120.java
@@ -31,4 +31,23 @@ public class LC120 {
         }
         return dp[0][0];
     }
+
+
+    public int minimumTotalSpaceOptimised(List<List<Integer>> triangle) {
+        // its a rule if you developed the recurrance realtion top down then the tablation should be bottom up
+        int m = triangle.size();
+        int[] dp = new int[m];
+        for(int i=0;i<m;i++){
+            dp[i] =  triangle.get(m-1).get(i);
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int cur = triangle.get(i).get(j);
+                int down = dp[j];
+                int diagonal = dp[j+1];
+                dp[j] = cur + Math.min(down,diagonal);
+            }
+        }
+        return dp[0];
+    }
 }
