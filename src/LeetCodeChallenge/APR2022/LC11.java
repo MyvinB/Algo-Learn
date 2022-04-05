78e5ea8e0103b932c45ad77583f8fb62a749f2d7
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-05 19:56:19 +0530

    DP +1 Dungeon Game

--- src/LeetCodeChallenge/APR2022/LC11.java
+++ src/LeetCodeChallenge/APR2022/LC11.java
@@ -1,6 +1,28 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+/**
+ * @author mmichaelbarboza
  * on 12:36 PM 4/5/2022
- * Time Taken: 
- */public class LC11 {
+ * Time Taken:
+ */
+public class LC11 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxArea(int[] height) {
+        int low = 0;
+        int high = height.length;
+        int maxArea = 0;
+        while(low<high){
+            int area = (high-low) * Math.min(height[low],height[high]);
+            maxArea = Math.max(area,maxArea);
+            if(height[low]<height[high]){
+                low = low +1;
+            }
+            else high = high-1;
+        }
+        return maxArea;
+    }
 }
---
+++ src/TopicWise2022/DP/MAXANDMIN/LC174.java
@@ -0,0 +1,43 @@
+package TopicWise2022.DP.MAXANDMIN;
+
+import java.util.Arrays;
+
+/**
+ * @author mmichaelbarboza
+ * on 7:31 PM 4/5/2022
+ * Time Taken:
+ */
+public class LC174 {
+
+    public static void main(String[] args) {
+        int[][] t = new int[][]{{-2,-3,3},{-5,-10,1},{10,30,-5}};
+        calculateMinimumHP(t);
+    }
+
+    public static int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[][] dp = new int[m][n];
+        //Last base case
+        dp[m-1][n-1] = Math.max(1,1-dungeon[m-1][n-1]);
+        //Calculating the single choice options
+        for(int i = m-2;i>=0;i--){
+            int cur = dungeon[i][n-1];
+            int prev = dp[i+1][n-1];
+            //if value is less than zero that means we have more power than rew make it one there cause that's all we need to live
+            dp[i][n-1] = Math.max(prev-cur,1);
+        }
+
+        for(int j=n-2;j>=0;j--){
+            int cur = dungeon[m-1][j];
+            int prev = dp[m-1][j+1];
+            dp[m-1][j] = Math.max(prev-cur,1);
+        }
+
+        System.out.println(Arrays.deepToString(dp));
+        return 0;
+
+    }
+
+
+}
