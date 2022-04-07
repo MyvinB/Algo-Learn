94cf1aac8756c36e46bed4483c6c3130c15ce511
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-07 07:59:39 +0530

    DP+2 Minimum Steps

--- src/TopicWise2022/DP/MAXANDMIN/LC474.java
+++ src/TopicWise2022/DP/MAXANDMIN/LC474.java
@@ -15,7 +15,9 @@ public class LC474 {
         System.out.println(findMaxForm(sAr,5,3));
     }
 //Trying Recursion
-    public static int findMaxForm(String[] strs, int m, int n) {
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   /*
+   public static int findMaxForm(String[] strs, int m, int n) {
         List<int[]> list = new ArrayList<>();
         for(String s:strs){
             int one=0;
@@ -45,4 +47,61 @@ public class LC474 {
 
         return Math.max(include,exclude);
     }
+
+    */
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Trying memoising it to calulate possibilities
+    public static int findMaxForm(String[] strs, int m, int n) {
+    List<int[]> list = new ArrayList<>();
+    for(String s:strs){
+        int one=0;
+        int zero=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0') zero++;
+            else one++;
+        }
+        list.add(new int[]{zero,one});
+    }
+    int[][][] dp = new int[601][101][101];
+    return dfs(list,0,m,n,dp);
+}
+
+    public static int dfs(List<int[]> countList,int index,int nZero,int nOne,int[][][] dp){
+        //Base condition
+        //1) If nZeros is equal to zero or nOne or countList end
+        if(index == countList.size() || nZero == 0 && nOne == 0) return 0;
+        if(dp[index][nZero][nOne] != 0) return dp[index][nZero][nOne];
+        int[] countAr = countList.get(index);
+
+        //If we do not have the required zeros or ones we skip that element
+        if(nZero<countAr[0] || nOne<countAr[1]){
+            return dfs(countList,index+1,nZero,nOne,dp);
+        }
+
+        int include = 1 + dfs(countList,index+1,nZero-countAr[0],nOne-countAr[1],dp);
+        int exclude = dfs(countList,index+1,nZero,nOne,dp);
+        dp[index][nZero][nOne] = Math.max(include,exclude);
+        return dp[index][nZero][nOne];
+    }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static int findMaxFormTabulation(String[] strs, int m, int n) {
+        int[][] dp = new int[101][101];
+        for(String s:strs){
+            int one=0;
+            int zero=0;
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='0') zero++;
+                else one++;
+            }
+            for(int i=m;i>=zero;i--){
+                for(int j=n;j>=one;j--){
+                    dp[i][j] = Math.max(dp[i][j],1 +dp[i-zero][j-one]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
 }
---
+++ src/TopicWise2022/DP/MAXANDMIN/LC650.java
@@ -0,0 +1,6 @@
+package TopicWise2022.DP.MAXANDMIN;/**
+ *@author mmichaelbarboza
+ * on 7:15 AM 4/7/2022
+ * Time Taken: 
+ */public class LC650 {
+}
