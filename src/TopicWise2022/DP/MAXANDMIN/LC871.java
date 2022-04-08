cc4dba168d4f57a99583b51757d99da8b1cd5b1d
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-08 07:54:37 +0530

    DP 2 Minimum falling path sum

--- src/TopicWise2022/DP/MAXANDMIN/LC871.java
+++ src/TopicWise2022/DP/MAXANDMIN/LC871.java
@@ -3,7 +3,7 @@ package TopicWise2022.DP.MAXANDMIN;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-/**
+/**+
  * @author mmichaelbarboza
  * on 6:53 AM 4/8/2022
  * Time Taken:
@@ -14,16 +14,37 @@ public class LC871 {
         int[][] t = new int[][]{{10,60},{20,30},{30,30},{60,40}};
         minRefuelStops(100,10,t);
     }
+    //DP O(n2)
+    public static int minRefuelStops(int target, int startFuel, int[][] s) {
+        long[] dp = new long[s.length+1];
+        dp[0] = startFuel;
+        for(int i=0;i<s.length;i++){
+            //dp[t]>=s[i][0] signifies we have enough petrol to reach spot
+            for(int t=i;t>=0 && dp[t]>=s[i][0];t--){
+                dp[t+1] = Math.max(dp[t+1],dp[t]+s[i][1]);
+            }
+        }
+        for(int i=0;i<=s.length;i++){
+            if(dp[i]>=target) return i;
+        }
+        return -1;
+    }
 
-    public static int minRefuelStops(int target, int cur, int[][] s) {
-        Queue<Integer> pq = new PriorityQueue<>();
-        int i = 0, res;
-        for (res = 0; cur < target; res++) {
-            while (i < s.length && s[i][0] <= cur)
-                pq.offer(-s[i++][1]);
-            if (pq.isEmpty()) return -1;
+    //PriorityQueue nlogn
+    public static int minRefuelStopsPq(int target, int startFuel, int[][] s) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        long cur = startFuel;
+        int i= 0;
+        int res = 0;
+        for(res = 0;cur<target;res++){
+            while(i<s.length && cur>=s[i][0]){
+                pq.add(-s[i++][1]);
+            }
+            if(pq.isEmpty()) return -1;
             cur += -pq.poll();
         }
         return res;
     }
+
+
 }
---
+++ src/TopicWise2022/DP/MAXANDMIN/LC931.java
@@ -0,0 +1,6 @@
+package TopicWise2022.DP.MAXANDMIN;/**
+ *@author mmichaelbarboza
+ * on 7:39 AM 4/8/2022
+ * Time Taken: 
+ */public class LC931 {
+}
