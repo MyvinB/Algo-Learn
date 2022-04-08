e43797cde31d1ba93c1f73d3c38de8df4b2372a7
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-08 09:12:43 +0530

    DP 3 LC983

--- src/TopicWise2022/DP/MAXANDMIN/LC931.java
+++ src/TopicWise2022/DP/MAXANDMIN/LC931.java
@@ -1,6 +1,35 @@
-package TopicWise2022.DP.MAXANDMIN;/**
- *@author mmichaelbarboza
+package TopicWise2022.DP.MAXANDMIN;
+
+/**
+ * @author mmichaelbarboza
  * on 7:39 AM 4/8/2022
- * Time Taken: 
- */public class LC931 {
+ * Time Taken:
+ */
+public class LC931 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            dp[0][i] = matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int left = j-1>=0?dp[i-1][j-1]:Integer.MAX_VALUE;
+                int middle = dp[i-1][j]; //No worry of overflow
+                int right =j+1<n? dp[i-1][j+1]:Integer.MAX_VALUE;
+                dp[i][j] = matrix[i][j]+Math.min(Math.min(left,middle),right);
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            min = Math.min(dp[n-1][i],min);
+        }
+        return min;
+    }
 }
---
+++ src/TopicWise2022/DP/MAXANDMIN/LC983.java
@@ -0,0 +1,6 @@
+package TopicWise2022.DP.MAXANDMIN;/**
+ *@author mmichaelbarboza
+ * on 8:32 AM 4/8/2022
+ * Time Taken: 
+ */public class LC983 {
+}
