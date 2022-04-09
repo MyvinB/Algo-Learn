e45ca09b1751fb2b34e9843c934885350e1d8cd0
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-09 08:56:10 +0530

    Daily challenege 04/09/2022

---
+++ src/LeetCodeChallenge/APR2022/LC347.java
@@ -0,0 +1,6 @@
+package LeetCodeChallenge.APR2022;/**
+ *@author mmichaelbarboza
+ * on 7:58 AM 4/9/2022
+ * Time Taken: 
+ */public class LC347 {
+}
--- src/TopicWise2022/DP/MAXANDMIN/LC983.java
+++ src/TopicWise2022/DP/MAXANDMIN/LC983.java
@@ -1,6 +1,34 @@
-package TopicWise2022.DP.MAXANDMIN;/**
- *@author mmichaelbarboza
+package TopicWise2022.DP.MAXANDMIN;
+
+/**
+ * @author mmichaelbarboza
  * on 8:32 AM 4/8/2022
- * Time Taken: 
- */public class LC983 {
+ * Time Taken:
+ */
+public class LC983 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int mincostTickets(int[] days, int[] costs) {
+        int lastDay = days[days.length-1];
+        int[] dp = new int[lastDay+1];
+        boolean isTravelDay[] = new boolean[lastDay+1];
+        for(int i=0;i<days.length;i++){
+            isTravelDay[days[i]] = true;
+        }
+        for(int i=0;i<=lastDay;i++){
+            if(!isTravelDay[i]){
+                //we copy over the values to the next element cause dp[i-1[ should have the precalculated values of the previous travlled day.
+                dp[i] = dp[i-1];
+                continue;
+            }
+
+            dp[i] = costs[0] +dp[i-1];
+            dp[i] = Math.min(dp[Math.max(i-7,0)]+costs[1],dp[i]);
+            dp[i] = Math.min(dp[Math.max(i-30,0)]+costs[2],dp[i]);
+        }
+        return dp[lastDay];
+    }
 }
