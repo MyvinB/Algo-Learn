39278db566f46feb057b6d24919981c118fba3ea
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-14 07:52:45 +0530

    LC DAILY

--- src/LeetCodeChallenge/APR2022/LC59.java
+++ src/LeetCodeChallenge/APR2022/LC59.java
@@ -1,6 +1,46 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+/**
+ * @author mmichaelbarboza
  * on 9:09 AM 4/13/2022
- * Time Taken: 
- */public class LC59 {
+ * Time Taken:
+ */
+public class LC59 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] generateMatrix(int n) {
+        int[][] t = new int[n][n];
+        int rowBegin = 0;
+        int rowEnd = n-1;
+        int colBegin = 0;
+        int colEnd = n-1;
+        int no =1;
+        while(rowBegin<=rowEnd && colBegin<=colEnd){
+            for(int i=colBegin;i<=colEnd;i++){
+                t[rowBegin][i] =no++;
+            }
+            rowBegin++;
+            for(int i=rowBegin;i<=rowEnd;i++){
+                t[i][colEnd] = no++;
+            }
+            colEnd--;
+            if(rowBegin<=rowEnd){
+                for(int i=colEnd;i>=colBegin;i--){
+                    t[rowEnd][i] = no++;
+                }
+            }
+            rowEnd--;
+            if(colBegin<=colEnd){
+                for(int i=rowEnd;i>=rowBegin;i--){
+                    t[i][colBegin] = no++;
+                }
+            }
+            colBegin++;
+
+        }
+        return t;
+    }
 }
---
+++ src/LeetCodeChallenge/APR2022/LC700.java
@@ -0,0 +1,6 @@
+package LeetCodeChallenge.APR2022;/**
+ *@author mmichaelbarboza
+ * on 7:34 AM 4/14/2022
+ * Time Taken: 
+ */public class LC700 {
+}
