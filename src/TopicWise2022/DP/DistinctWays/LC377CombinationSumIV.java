b6f6bb6005174ddcf2e0e6db05ae08213cbf27de
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-14 17:20:03 +0530

    Yayie canPartion :)

--- src/TopicWise2022/DP/DistinctWays/LC377CombinationSumIV.java
+++ src/TopicWise2022/DP/DistinctWays/LC377CombinationSumIV.java
@@ -15,11 +15,11 @@ public class LC377CombinationSumIV {
     public static int combinationSum4(int[] nums, int target) {
         int n = nums.length;
         int[] dp = new int[target+1];
-        dp[0] = 0;
+        dp[0] = 1;
         for(int i=1;i<=target;i++){
             for(int j=0;j<n;j++){
                 if(i-nums[j]>=0){
-                  dp[i] += dp[i-nums[j]] +1;
+                  dp[i] += dp[i-nums[j]];
                 }
             }
         }
---
+++ src/TopicWise2022/DP/DistinctWays/LC416PartionSubset.java
@@ -0,0 +1,20 @@
+package TopicWise2022.DP.DistinctWays;
+
+/**
+ * @author mmichaelbarboza
+ * on 10:47 AM 4/14/2022
+ * Time Taken:
+ */
+public class LC416PartionSubset {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int i=0;i<nums.length;i++) sum+= nums[i];
+
+
+    }
+}
