f1196d832eb36c0bd1fcc7854c8498439a7b78e5
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-14 08:01:44 +0530

    Unique Path 1

--- src/LeetCodeChallenge/APR2022/LC700.java
+++ src/LeetCodeChallenge/APR2022/LC700.java
@@ -1,6 +1,22 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+import TopicWise2022.Tree.common.TreeNode;
+
+/**
+ * @author mmichaelbarboza
  * on 7:34 AM 4/14/2022
- * Time Taken: 
- */public class LC700 {
+ * Time Taken:
+ */
+public class LC700 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null) return null;
+        if(val>root.val) return searchBST(root.right,val);
+        else if(val<root.val) return searchBST(root.left,val);
+        return root;
+    }
 }
---
+++ src/TopicWise2022/DP/DistinctWays/LC62UniquePath.java
@@ -0,0 +1,6 @@
+package TopicWise2022.DP.DistinctWays;/**
+ *@author mmichaelbarboza
+ * on 7:54 AM 4/14/2022
+ * Time Taken: 
+ */public class LC62UniquePath {
+}
