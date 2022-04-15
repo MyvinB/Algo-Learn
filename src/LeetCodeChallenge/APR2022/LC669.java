c3bbe61d66df22f1622aa0bf475d5fb29bb002a7
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-15 19:29:55 +0530

    lc494 tARGET SUM

--- src/LeetCodeChallenge/APR2022/LC669.java
+++ src/LeetCodeChallenge/APR2022/LC669.java
@@ -1,6 +1,25 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+import TopicWise2022.Tree.common.TreeNode;
+
+/**
+ * @author mmichaelbarboza
  * on 6:00 AM 4/15/2022
- * Time Taken: 
- */public class LC669 {
+ * Time Taken:
+ */
+public class LC669 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root==null) return null;
+
+        if(root.val<low) return trimBST(root.right,low,high);
+        if(root.val>high) return trimBST(root.left,low,high);
+        root.left = trimBST(root.left,low,high);
+        root.right = trimBST(root.right,low,high);
+        return root;
+    }
 }
---
+++ src/TopicWise2022/DP/DistinctWays/LC494TargetSum.java
@@ -0,0 +1,6 @@
+package TopicWise2022.DP.DistinctWays;/**
+ *@author mmichaelbarboza
+ * on 6:54 PM 4/15/2022
+ * Time Taken: 
+ */public class LC494TargetSum {
+}
