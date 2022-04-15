331ad983bc99f559026400e08167f61a097d458f
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-15 06:05:38 +0530

    LC Daily lEETCODE

---
+++ src/LeetCodeChallenge/APR2022/LC669.java
@@ -0,0 +1,6 @@
+package LeetCodeChallenge.APR2022;/**
+ *@author mmichaelbarboza
+ * on 6:00 AM 4/15/2022
+ * Time Taken: 
+ */public class LC669 {
+}
--- src/TopicWise2022/DP/DistinctWays/LC416PartionSubset.java
+++ src/TopicWise2022/DP/DistinctWays/LC416PartionSubset.java
@@ -14,7 +14,16 @@ public class LC416PartionSubset {
     public boolean canPartition(int[] nums) {
         int sum = 0;
         for(int i=0;i<nums.length;i++) sum+= nums[i];
-
-
+        if(sum%2!=0) return false;
+        sum = sum/2;
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+        for(int i=1;i<=nums.length;i++){
+            for(int j=sum;j>=0;j--){
+                if(j>=nums[i-1])
+                    dp[j] = dp[j] || dp[j-nums[i-1]];
+            }
+        }
+        return dp[sum];
     }
 }
