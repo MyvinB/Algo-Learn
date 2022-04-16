8a14bbd6eac9e61719d179743d8657b5e7e529f5
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-16 15:04:57 +0530

    3d dp

--- src/LeetCodeChallenge/APR2022/LC538.java
+++ src/LeetCodeChallenge/APR2022/LC538.java
@@ -1,6 +1,28 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+import TopicWise2022.Tree.common.TreeNode;
+
+/**
+ * @author mmichaelbarboza
  * on 2:05 PM 4/16/2022
- * Time Taken: 
- */public class LC538 {
+ * Time Taken:
+ */
+public class LC538 {
+
+    public static void main(String[] args) {
+
+    }
+
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root!=null){
+            convertBST(root.right);
+            sum+=root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+
+
+
 }
