94016e476d44199ac5286b7b919d59cf5849c47b
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-20 22:56:33 +0530

    LC173 inorder traversal of binary tree

---
+++ src/LeetCodeChallenge/APR2022/LC173BinarySearchTreeIterator.java
@@ -0,0 +1,6 @@
+package LeetCodeChallenge.APR2022;/**
+ *@author mmichaelbarboza
+ * on 10:55 PM 4/20/2022
+ * Time Taken: 
+ */public class LC173BinarySearchTreeIterator {
+}
--- src/LeetCodeChallenge/APR2022/LC99.java
+++ src/LeetCodeChallenge/APR2022/LC99.java
@@ -1,6 +1,40 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+import TopicWise2022.Tree.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @author mmichaelbarboza
  * on 12:10 PM 4/19/2022
- * Time Taken: 
- */public class LC99 {
+ * Time Taken:
+ */
+public class LC99 {
+    public static void main(String[] args) {
+
+    }
+    TreeNode prev;
+    TreeNode one;
+    TreeNode two;
+    public void recoverTree(TreeNode root) {
+       inorder(root);
+       int temp = one.val;
+       one.val = two.val;
+       two.val = temp;
+    }
+
+    private void inorder(TreeNode cur){
+        if(cur==null) return;
+        //DIp condition
+        inorder(cur.left);
+        if(prev!=null && prev.val>cur.val){
+            if(one==null){
+                one = prev;
+            }
+            two=cur;
+        }
+        prev=cur;
+        inorder(cur.right);
+    }
 }
