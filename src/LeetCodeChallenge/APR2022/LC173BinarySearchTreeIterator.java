313f1543016a799e8007238018c776e9808f743a
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-21 11:36:58 +0530

    Testing

--- src/LeetCodeChallenge/APR2022/LC173BinarySearchTreeIterator.java
+++ src/LeetCodeChallenge/APR2022/LC173BinarySearchTreeIterator.java
@@ -1,6 +1,41 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+import TopicWise2022.Tree.common.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author mmichaelbarboza
  * on 10:55 PM 4/20/2022
- * Time Taken: 
- */public class LC173BinarySearchTreeIterator {
+ * Time Taken:
+ */
+public class LC173BinarySearchTreeIterator {
+    public static void main(String[] args) {
+
+    }
+
+    class BSTIterator {
+
+        Stack<TreeNode> stack = null;
+        TreeNode cur = null;
+
+        public BSTIterator(TreeNode root) {
+            cur = root;
+            stack = new Stack<TreeNode>();
+        }
+
+        public int next() {
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            TreeNode t = stack.pop();
+            cur = t.right;
+            return t.val;
+        }
+
+        public boolean hasNext() {
+            return cur!=null || !stack.isEmpty();
+        }
+    }
 }
