8eebd30b6dc59805218942ec3960ea265ecab31c
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-23 15:32:39 +0530

    LC535 LC Daily design

---
+++ src/LeetCodeChallenge/APR2022/LC535EncodeAndDecodeTinyURL.java
@@ -0,0 +1,6 @@
+package LeetCodeChallenge.APR2022;/**
+ *@author mmichaelbarboza
+ * on 3:27 PM 4/23/2022
+ * Time Taken: 
+ */public class LC535EncodeAndDecodeTinyURL {
+}
--- src/TopicWise2022/DP/DistinctWays/LC790DominoAndTrominoTiling.java
+++ src/TopicWise2022/DP/DistinctWays/LC790DominoAndTrominoTiling.java
@@ -1,6 +1,26 @@
-package TopicWise2022.DP.DistinctWays;/**
- *@author mmichaelbarboza
+package TopicWise2022.DP.DistinctWays;
+
+/**
+ * @author mmichaelbarboza
  * on 12:38 AM 4/23/2022
- * Time Taken: 
- */public class LC790DominoAndTrominoTiling {
+ * Time Taken:
+ */
+public class LC790DominoAndTrominoTiling {
+
+    public static void main(String[] args) {
+
+    }
+    //Simple math formula need to understand logic
+    public int numTilings(int n) {
+        int mod = 1000000007;
+        if(n<3) return n;
+        int[] dp = new int[n+1];
+        dp[0]= 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = ((2*dp[i-1])%mod + dp[i-3])%mod;
+        }
+        return dp[n];
+    }
 }
