5734b7469a8b6c2e4061caefd7ce50bfb4f03d83
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-23 15:42:43 +0530

    Changes

--- src/LeetCodeChallenge/APR2022/LC535EncodeAndDecodeTinyURL.java
+++ src/LeetCodeChallenge/APR2022/LC535EncodeAndDecodeTinyURL.java
@@ -1,6 +1,32 @@
-package LeetCodeChallenge.APR2022;/**
- *@author mmichaelbarboza
+package LeetCodeChallenge.APR2022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mmichaelbarboza
  * on 3:27 PM 4/23/2022
- * Time Taken: 
- */public class LC535EncodeAndDecodeTinyURL {
+ * Time Taken:
+ */
+public class LC535EncodeAndDecodeTinyURL {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Codec {
+
+        List<String> res = new ArrayList<>();
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            res.add(longUrl);
+            return String.valueOf(res.size()-1);
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            int index = Integer.valueOf(shortUrl);
+            return index<res.size()?res.get(index):"";
+        }
+    }
 }
