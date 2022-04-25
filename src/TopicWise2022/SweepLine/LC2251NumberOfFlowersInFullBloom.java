7fa1c50ba4507e994ca11a4a5b2bc3063ef25fff
Myvin Barboza Michael Barboza <myvinbarboza@gmail.com>
2022-04-25 06:50:19 +0530

    lC 2251 Sweep line algo contest

---
+++ src/TopicWise2022/SweepLine/LC2251NumberOfFlowersInFullBloom.java
@@ -0,0 +1,6 @@
+package TopicWise2022.SweepLine;/**
+ *@author mmichaelbarboza
+ * on 6:38 AM 4/25/2022
+ * Time Taken: 
+ */public class LC2251NumberOfFlowersInFullBloom {
+}
--- src/TopicWise2022/SweepLine/MeetingsRoomII.java
+++ src/TopicWise2022/SweepLine/MeetingsRoomII.java
@@ -1,6 +1,48 @@
-package TopicWise2022.SweepLine;/**
- *@author mmichaelbarboza
+package TopicWise2022.SweepLine;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author mmichaelbarboza
  * on 6:16 AM 4/25/2022
- * Time Taken: 
- */public class MeetingsRoomII {
+ * Time Taken:
+ */
+public class MeetingsRoomII {
+    public class Interval {
+      int start, end;
+      Interval(int start, int end) {
+          this.start = start;
+          this.end = end;
+      }
+    }
+
+
+    public int minMeetingRooms(List<Interval> intervals) {
+        int n = intervals.size();
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for(int i=0;i<intervals.size();i++){
+            Interval temp = intervals.get(i);
+            start[i] = temp.start;
+            end[i] = temp.end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int s=0,e=0,count=0,res=0;
+        while(s<n){
+            if(start[s]<end[e]){
+                s++;
+                count++;
+            }
+            //If meeting start time and end time is same we first end the meeting hence end<=start is the condition here
+            else{
+                e++;
+                count--;
+            }
+            res = Math.max(res,count);
+
+        }
+        return res;
+    }
 }
