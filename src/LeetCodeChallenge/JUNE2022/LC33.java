313f26a9b98432cce4bdcbf088c8df6951918f11
myvinb <myvinbarboza@gmail.com>
2022-06-15 14:42:48 +0530

    Search in a roated sortted array

---
+++ src/LeetCodeChallenge/JUNE2022/LC33.java
@@ -0,0 +1,24 @@
+package LeetCodeChallenge.JUNE2022;
+
+public class LC33 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int search(int[] nums, int target) {
+        int lo = 0,hi=nums.length-1;
+        while(lo<hi){
+            int mid = lo+ (hi-lo)/2;
+            if(nums[lo]<=nums[mid]){
+                if(target<nums[lo] || target>nums[mid]) lo=mid+1;
+                else hi = mid;
+            }else {
+                if(target<nums[mid] || target>nums[hi]) hi=mid-1;
+                else lo=mid;
+            }
+        }
+        if(nums[lo]==target) return lo;
+        return -1;
+    }
+}
