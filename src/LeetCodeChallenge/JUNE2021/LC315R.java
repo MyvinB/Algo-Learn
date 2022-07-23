67027de97a3a1347dfec1cd9e27401c7a77e7204
myvinb <myvinbarboza@gmail.com>
2022-07-23 15:48:15 +0530

    Segment Tree Count number

--- src/LeetCodeChallenge/JUNE2021/LC315R.java
+++ src/LeetCodeChallenge/JUNE2021/LC315R.java
@@ -1,11 +1,17 @@
-package Templates;
+package LeetCodeChallenge.JUNE2021;
 
+import Templates.SegmentTreeTemplate;
 
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SegmentTreeTemplate {
+public class LC315R {
 
     public static void main(String[] args) {
-        
+        int[] t = new int[]{5,2,6,1};
+        LC315R as = new LC315R();
+        System.out.println(as.countSmaller(t));
     }
     SegmentTree root = null;
     class SegmentTree{
@@ -25,21 +31,19 @@ public class SegmentTreeTemplate {
         SegmentTree root = new SegmentTree(start,end);
         //leaf nodes
         if(start==end){
-            root.sum = nums[start];
+            return root;
         }
         else {
             int mid = start+ (end-start)/2;
             root.left = buildTree(nums,start,mid);
             root.right = buildTree(nums,mid+1,end);
-            root.sum = root.left.sum +root.right.sum;
         }
-
         return root;
     }
 
     void updateHelper(SegmentTree root, int index, int val){
         if(root.start == root.end){
-            root.sum = val;
+            root.sum += val;
         }
         else {
             int mid = root.start + (root.end-root.start)/2;
@@ -52,6 +56,9 @@ public class SegmentTreeTemplate {
     }
 
     int sumRange(SegmentTree root, int start, int end){
+        if (root == null || start >end) {
+            return 0;
+        }
         if(root.start== start && root.end==end) return root.sum;
         else {
             int mid = root.start +(root.end-root.start)/2;
@@ -65,5 +72,24 @@ public class SegmentTreeTemplate {
         }
     }
 
+
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        int[] counts = new int[n];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            min = Math.min(min,nums[i]);
+            max = Math.max(max,nums[i]);
+        }
+        SegmentTree segmentTree = buildTree(nums,min,max);
+        for(int i=n-1;i>=0;i--){
+            updateHelper(segmentTree,nums[i],1);
+            counts[i] = sumRange(segmentTree,min,nums[i]-1);
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<counts.length;i++) res.add(counts[i]);
+        return res;
+    }
 
 }
