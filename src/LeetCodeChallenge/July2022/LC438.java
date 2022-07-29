42d6221b0145b033f72bd159e7e429ba10e6d172
myvinb <myvinbarboza@gmail.com>
2022-07-29 10:52:37 +0530

    LC438 find pattern

---
+++ src/LeetCodeChallenge/July2022/LC438.java
@@ -0,0 +1,37 @@
+package LeetCodeChallenge.July2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class LC438 {
+
+    public static void main(String[] args) {
+       // "abb"
+        System.out.println(Arrays.toString(findPattern("baba")));
+    }
+
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+        int[] toGet = findPattern(pattern);
+        for(String word:words){
+            if(Arrays.equals(toGet,findPattern(word))){
+                res.add(word);
+            }
+        }
+        return res;
+    }
+
+    public static int[] findPattern(String pattern){
+        int n = pattern.length();
+        HashMap<Character,Integer> map = new HashMap<>();
+        int[] res = new int[n];
+        for(int i=0;i<n;i++){
+            char ch = pattern.charAt(i);
+            map.putIfAbsent(ch,map.size());
+            res[i] = map.get(ch);
+        }
+        return res;
+    }
+}
