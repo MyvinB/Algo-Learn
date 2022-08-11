6e96d14714838a56308837bd9ac1bc8089b11240
myvinb <myvinbarboza@gmail.com>
2022-08-11 17:49:21 +0530

    Bleh timepass

---
+++ src/LeetCodeChallenge/Aug2022/DpPractise/LC70.java
@@ -0,0 +1,16 @@
+package LeetCodeChallenge.Aug2022.DpPractise;
+
+public class LC70 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        if(n>=1) dp[1] = 1;
+        if(n>=2) dp[2] = 2;
+        for(int i=3;i<=n;i++)dp[i] = dp[i-1]+dp[i-2];
+        return dp[n];
+    }
+}
