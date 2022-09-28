bca2c799ebfddfdff227d30e54a6210fe8fbabb6
myvinb <myvinbarboza@gmail.com>
2022-09-28 20:20:51 +0530

    Matrix Multiplication

---
+++ src/LeetCodeChallenge/Sep2022/DpPractise/PartionDp/MatrixMultiplication.java
@@ -0,0 +1,29 @@
+package LeetCodeChallenge.Sep2022.DpPractise.PartionDp;
+
+import java.util.Arrays;
+
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int matrixMultiplication(int[] arr , int N) {
+        int[][] dp = new int[N][N];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        return recur(arr,1,N-1, dp);
+    }
+
+    public static int recur(int[] arr,int i,int j,int[][] dp){
+        //base case
+        if(i==j) return 0;
+        if(dp[i][j] !=-1) return dp[i][j];
+        int min = Integer.MAX_VALUE;
+        for(int k=i;k<j;k++){
+                        //calcualting final operations
+            int ans = arr[i-1]*arr[k]*arr[j] + recur(arr,i,k,dp) + recur(arr,k+1,j,dp);
+            min = Math.min(min,ans);
+        }
+        return dp[i][j] = min;
+    }
+}
