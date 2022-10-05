e33cd51170c2f39f18e834d9506ddbd36ec9fb73
myvinb <myvinbarboza@gmail.com>
2022-10-05 22:59:14 +0530

    Not bad LC21

---
+++ src/LeetCodeChallenge/Oct2022/LinkedList/LC21.java
@@ -0,0 +1,37 @@
+package LeetCodeChallenge.Oct2022.LinkedList;
+
+import TopicWise2022.common.ListNode;
+
+public class LC21 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+    //-1--> 1->1->2->3->3
+    // 1 ->2 3
+    // ->1 3 4
+    public ListNode mergeList(ListNode l1,ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while(l1!=null || l2!=null){
+            if(l1==null){
+                cur.next =  l2;
+                break;
+            }
+            if(l2==null){
+                cur.next =  l1;
+                break;
+            }
+            if(l1.val<=l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
