e7ebe0e0c2135e5cc0e140b83422dd95839a9059
Barboza <myvinbarboza@gmail.com>
2022-10-23 11:19:03 +0530

    Pre Diwali sliding window

---
+++ src/LeetCodeChallenge/Oct2022/Daily/LC3.java
@@ -0,0 +1,26 @@
+package LeetCodeChallenge.Oct2022.Daily;
+
+public class LC3 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[128];
+        int lo=0;int start = -1,end =-1;
+        int maxLen = 0;
+        for(int hi=0;hi<s.length();hi++){
+            map[s.charAt(hi)]++;
+
+            while(map[s.charAt(hi)]>1){
+                map[s.charAt(lo)]--;
+                lo++;
+            }
+            if(hi-lo+1>maxLen){
+                maxLen = hi-lo+1;
+            }
+        }
+        return maxLen;
+    }
+}
