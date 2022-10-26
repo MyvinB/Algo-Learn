189e7382db9551b3ff5c7418cad7ae784848e5ae
Barboza <myvinbarboza@gmail.com>
2022-10-26 10:30:59 +0530

    Flood fill timepass

--- src/LeetCodeChallenge/Oct2022/Graph/FloodFillAlgorithm.java
+++ src/LeetCodeChallenge/Oct2022/Graph/FloodFillAlgorithm.java
@@ -10,7 +10,20 @@ public class FloodFillAlgorithm {
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        return null;
+        int[][] res = image;
+        dfs(image,image[sr][sc],sr,sc,newColor);
+        return res;
+    }
+
+    public void dfs(int[][] image,int intialColor, int sr,int sc,int newColor){
+        int[][] dir = new int[][]{{1,0},{-1,0},{0,-1},{0,1}};
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length || image[sr][sc]!=intialColor ||image[sr][sc]==newColor) return;
+        image[sr][sc] = newColor;
+        for(int[] d:dir){
+            int x = sr+d[0];
+            int y = sc+d[1];
+            dfs(image,intialColor,x,y,newColor);
+        }
     }
 }
 
