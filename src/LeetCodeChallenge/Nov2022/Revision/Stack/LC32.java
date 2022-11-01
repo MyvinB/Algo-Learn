d9477534c8be6f196adc97ee2888adc37febd6fa
Barboza <myvinbarboza@gmail.com>
2022-11-01 19:44:01 +0530

    Revison

---
+++ src/LeetCodeChallenge/Nov2022/Revision/Stack/LC32.java
@@ -0,0 +1,32 @@
+package LeetCodeChallenge.Nov2022.Revision.Stack;
+
+import java.util.Stack;
+
+public class LC32 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int left = -1,max = 0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }else{
+                //We are storing the index of the  ) as its invalid and there is nothing to pop from stack
+                if(stack.isEmpty()) left = i;
+                else {
+                    stack.pop();
+                    if(stack.isEmpty()) max = Math.max(max,i-left); // here we take the last closed bracket position
+                    else max = Math.max(max,i-stack.peek()); //here we see first invalid open bracket
+                }
+
+            }
+        }
+        return max;
+    }
+}
+
+
