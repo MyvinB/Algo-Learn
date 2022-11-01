cc6122b55a7d3a86c4a3eca8b66215956bc2e6b4
Barboza <myvinbarboza@gmail.com>
2022-11-01 20:31:28 +0530

    LC84

--- src/LeetCodeChallenge/Nov2022/Revise/LC84.java
+++ src/LeetCodeChallenge/Nov2022/Revise/LC84.java
@@ -1,48 +1,45 @@
-package LeetCodeChallenge.Oct2022.Stack;
+package LeetCodeChallenge.Nov2022.Revise;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 public class LC84 {
 
     public static void main(String[] args) {
-        int[] t = new int[]{1,0};
-        System.out.println(largestRectangleAreaOptimised(t));
+
     }
-    //tghink of boundary as plus
-    public static int largestRectangleArea(int[] heights) {
+    //using leftMin+1 and rightMin-1
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
         int n = heights.length;
         int[] left = new int[n];
         int[] right = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<heights.length;i++){
-            while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]) stack.pop();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && heights[stack.peek()]>heights[i]) stack.pop();
             if(stack.isEmpty()) left[i] = 0;
-            else left[i] = stack.peek()+1; //When we find the lower element we need +1 index cause there we will start from
+            else left[i]=stack.peek()+1;
             stack.push(i);
         }
-        System.out.println(Arrays.toString(left));
         stack.clear();
         for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]) stack.pop();
+            while(!stack.isEmpty() && heights[stack.peek()]>heights[i]) stack.pop();
             if(stack.isEmpty()) right[i] = n-1;
             else right[i] = stack.peek()-1;
             stack.push(i);
         }
-        System.out.println(Arrays.toString(right));
-        int maxArea = 0;
+        int area = 0;
         for(int i=0;i<n;i++){
-            int area = (right[i]-left[i]+1) *heights[i];
-            maxArea = Math.max(maxArea,area);
+            int curArea = (right[i]-left[i]+1)*heights[i];
+            area = Math.max(area,curArea);
         }
-        return maxArea;
+        return area;
     }
 
-    public static int largestRectangleAreaOptimised(int[] heights) {
+
+    public int largestRectangleAreaOpti(int[] heights) {
         Stack<Integer> stack = new Stack<>();
         int n = heights.length;
         int area = 0;
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<n;i++){
             while(!stack.isEmpty() &&( i==n|| heights[stack.peek()]>heights[i] )){
                 int height = heights[stack.pop()];
                 int width;
